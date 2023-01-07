88231d213215501938a8d479920842ba78af7964
Md. Kamrul Hasan <icthasan36@gmail.com>
2023-01-07 12:24:24 +0600

    Validate image file, upload image file, fetch image file, edit image file

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
@@ -7,15 +8,17 @@ const postRoutes = require('./routes/posts')
 const app = express();
 
 mongoose.connect('mongodb+srv://Hasan1971:730025@cluster0.2greaqi.mongodb.net/mean?retryWrites=true&w=majority')
-.then((response)=>{
-  //console.log("Connected to Database", response);
-})
-.catch((err)=>{
-  console.log("Error during connecting to Database", err);
-})
+  .then((response) => {
+    //console.log("Connected to Database", response);
+  })
+  .catch((err) => {
+    console.log("Error during connecting to Database", err);
+  })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/images", express.static(path.join("backend/images")))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -25,11 +28,11 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
+    'GET,PUT, POST, PATCH, DELETE, OPTIONS'
   );
   next();
 });
 
-app.use("/api/posts",postRoutes);
+app.use("/api/posts", postRoutes);
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http"
 
-import {MatFormFieldModule} from '@angular/material/form-field';
+import  {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from "@angular/material/input";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
@@ -28,7 +28,7 @@ import { AppRoutingModule } from "./app-routing.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
