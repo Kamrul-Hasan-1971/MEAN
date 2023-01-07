236daa879675fb2b07a77708e5753b0b7e9f96b4
Md. Kamrul Hasan <icthasan36@gmail.com>
2023-01-07 15:07:26 +0600

    Add pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatPaginatorModule } from "@angular/material/paginator";
+
 
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
@@ -37,6 +39,7 @@ import { AppRoutingModule } from "./app-routing.module";
     MatExpansionModule,
     MatFormFieldModule,
     MatProgressSpinnerModule,
+    MatPaginatorModule,
     HttpClientModule,
   ],
   providers: [],
